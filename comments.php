38ee6e24c115f5a6d2c35bc329eb3b6a89d850dc
Francois MARI <francois.mari75@icloud.com>
2018-04-25 00:16:12 +0200

    Comments File
    No description

---
+++ comments.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+$string = file_get_contents($_SESSION['URL']."/comments.json");
+$json = json_decode($string, true);
+?>
+<html>
+	<head>
+		<title>Comments - Instagram Data</title>
+		<?php
+		include('includes/head.php');
+		?>
+	</head>
+	
+	<body>
+		
+		<div class="container">
+			<br>
+			<br>
+			<br>
+			<div align="center">
+			<h1><strong><span class="fa fa-instagram"></span> Discover more of your Instagram</strong></h1>
+			<p class="lead">Check all the data that Instagram has from you</p>
+			<br>
+			<hr>
+			</div>
+			<a href="data.php" class="btn btn-primary"><span class="fa fa-chevron-left"></span> Back home</a><br><br>
+			<p class="lead">You need to upload the file "comments.json" in the json folder to see the comments below.</p>
+			<h4><strong>Comments (order by date) <?= $_SESSION['URL'] ?></strong></h4>
+			<br>
+
+			<?php
+			foreach ($json as $key => $value) {
+    if (!is_array($value)) {
+        echo $key . '=>' . $value . '<br />';
+    } else {
+        foreach ($value as $key => $val) {
+            echo 'N<sup>o</sup>'.$key.' - To '.$val[2].' : ' . $val[1] . '<br>Date : '.$val[0].'<br><hr>';
+        }
+    }
+}		
+			?>
+
+		</div>
+		
+		<footer>
+			<div class="container">
+			<br>
+			<p class="lead">© François MARI 2018 - Not affiliated with Instagram</p>
+			</div>
+		</footer>
+		
+	</body>
+	
+</html>
