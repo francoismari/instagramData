dc0d44699f66c3cb4682656e728a401e70229972
Francois MARI <francois.mari75@icloud.com>
2018-04-25 13:06:55 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<div class="container">
+		<br>
+		<p class="lead">© @frrancois_ - Not affiliated with Instagram. <a href="//twitter.com/frrancois_" target="_blank" style="text-decoration: none"><span class="fa fa-twitter"></span> @frrancois_</a></p>
+</div>
