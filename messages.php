b537ff676946f5f130e9bd9ed277d08ae827a819
Francois MARI <francois.mari75@icloud.com>
2018-04-25 11:37:42 +0200

    Update messages.php

--- messages.php
+++ messages.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+include('session.php');
 $url = $_SESSION['URL'].'/messages.json'; // path to your JSON file
 $data = file_get_contents($url); // put the contents of the file into a variable
 $characters = json_decode($data); // decode the JSON feed
