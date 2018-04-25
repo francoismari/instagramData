608c8aca59d1d0b5a4baee6c898b1a06e66f7e5f
François MARI <francois.mari75@icloud.com>
2018-04-25 12:39:52 +0200

    Update twitter footer link

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <div class="container">
 		<br>
-		<p class="lead">© François MARI 2018 - Not affiliated with Instagram. <a href="//twitter.com/frrancois_" style="text-decoration: none"><span class="fa fa-twitter"></span> @frrancois_</a></p>
+		<p class="lead">© @frrancois_ - Not affiliated with Instagram. <a href="//twitter.com/frrancois_" target="_blank" style="text-decoration: none"><span class="fa fa-twitter"></span> @frrancois_</a></p>
 </div>
--- index.php
+++ index.php
@@ -62,7 +62,7 @@ if(isset($_POST['access']))
 		
 		<footer>
 			<div class="container">
-			<p class="lead">© François MARI 2018 - Not affiliated with Instagram. <a href="//twitter.com/frrancois_" style="text-decoration: none"><span class="fa fa-twitter"></span> @frrancois_</a></p>
+			<p class="lead">© @frrancois_ 2018 - Not affiliated with Instagram. <a href="//twitter.com/frrancois_" target="_blank" style="text-decoration: none"><span class="fa fa-twitter"></span> @frrancois_</a></p>
 			</div>
 		</footer>
 	</body>
