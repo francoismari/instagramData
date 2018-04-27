13e2b742d865499facb3cdbd9066108b5658801d
François MARI <francois.mari75@icloud.com>
2018-04-27 15:58:35 +0200

    Update the footer (add the GitHub link)

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <div class="container">
 		<br>
-		<p class="lead">© @frrancois_ - Not affiliated with Instagram. <a href="//twitter.com/frrancois_" target="_blank" style="text-decoration: none"><span class="fa fa-twitter"></span> @frrancois_</a></p>
+		<p class="lead">© @frrancois_ - Not affiliated with Instagram. <a href="//twitter.com/frrancois_" target="_blank" style="text-decoration: none"><span class="fa fa-twitter"></span> @frrancois_</a>&nbsp<a href="//github.com/francoismari/InstagramData" target="_blank" style="text-decoration: none"><span class="fa fa-github"></span> Visit the GitHub</a></p>
 </div>
