cc4f181a76c3896b56fbdf6b4b177985b5ba792b
François MARI <francois.mari75@icloud.com>
2018-04-27 14:54:45 +0200

    Update index.php footer (add the GitHub link)

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ if(isset($_POST['access']))
 		
 		<footer>
 			<div class="container">
-			<p class="lead">© @frrancois_ 2018 - Not affiliated with Instagram. <a href="//twitter.com/frrancois_" target="_blank" style="text-decoration: none"><span class="fa fa-twitter"></span> @frrancois_</a></p>
+			<p class="lead">© @frrancois_ - Not affiliated with Instagram. <a href="//twitter.com/frrancois_" target="_blank" style="text-decoration: none"><span class="fa fa-twitter"></span> @frrancois_</a>&nbsp<a href="//github.com/francoismari/InstagramData" target="_blank" style="text-decoration: none"><span class="fa fa-github"></span> Visit the GitHub</a></p>
 			</div>
 		</footer>
 	</body>
